099bb22bb43257049380ea23a14d44f450a0b14e
Olu Isaac <oluisaac7@gmail.com>
2019-03-27 19:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+    return kittens.push(name)
+}
+
+function destructivelyApendKitten(name){
+    return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+    return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+    return kittens.shift()
+}
+
+function AppendKitten(name){
+    return [...kittens, name]
+}
+
+function AppendKitten(name){
+    return [name, ...kittens]
+}
+
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+    return kittens.slice(1)
+}
